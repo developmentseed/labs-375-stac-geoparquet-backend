e59c425c7023177250b964d7d4c78a1dc17155b0
Pete Gadomski <pete.gadomski@gmail.com>
2025-01-03 09:32:29 -0700

    fix: don't import settings in main

--- src/tistac/main.py
+++ src/tistac/main.py
@@ -2,13 +2,9 @@ import importlib.metadata
 
 from fastapi import FastAPI
 
-import tistac.dependencies
 import tistac.router
 
-settings = tistac.dependencies.get_settings()
 app = FastAPI(
-    title=settings.catalog_title or settings.catalog_id,
-    description=settings.catalog_description,
     version=importlib.metadata.distribution("tistac").version,
 )
 app.include_router(tistac.router.router)
