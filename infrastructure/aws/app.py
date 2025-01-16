a8de845c0dd6b2730bdce83a4b3541df54ea5f40
hrodmn <henry.rodman@gmail.com>
2025-01-16 10:20:25 -0600

    incorporate PR review feedback

--- infrastructure/aws/app.py
+++ infrastructure/aws/app.py
@@ -16,14 +16,19 @@ from aws_cdk import (
     RemovalPolicy,
     Stack,
     Tags,
-    aws_iam,
-    aws_lambda,
-    aws_logs,
-    aws_s3,
-    custom_resources,
 )
 from aws_cdk.aws_apigatewayv2 import HttpApi, HttpStage, ThrottleSettings
 from aws_cdk.aws_apigatewayv2_integrations import HttpLambdaIntegration
+from aws_cdk.aws_iam import AnyPrincipal, Effect, PolicyStatement
+from aws_cdk.aws_lambda import Code, Function, Runtime
+from aws_cdk.aws_logs import RetentionDays
+from aws_cdk.aws_s3 import BlockPublicAccess, Bucket
+from aws_cdk.custom_resources import (
+    AwsCustomResource,
+    AwsCustomResourcePolicy,
+    AwsSdkCall,
+    PhysicalResourceId,
+)
 from config import Config
 from constructs import Construct
 
@@ -34,7 +39,7 @@ class StacFastApiGeoparquetStack(Stack):
         scope: Construct,
         construct_id: str,
         config: Config,
-        runtime: aws_lambda.Runtime = aws_lambda.Runtime.PYTHON_3_12,
+        runtime: Runtime = Runtime.PYTHON_3_12,
         **kwargs: Any,
     ) -> None:
         super().__init__(scope, construct_id, **kwargs)
@@ -42,7 +47,7 @@ class StacFastApiGeoparquetStack(Stack):
         for key, value in config.tags.items():
             Tags.of(self).add(key, value)
 
-        bucket = aws_s3.Bucket(
+        bucket = Bucket(
             scope=self,
             id="bucket",
             bucket_name=config.bucket_name,
@@ -51,7 +56,7 @@ class StacFastApiGeoparquetStack(Stack):
             if config.stage != "test"
             else RemovalPolicy.DESTROY,
             public_read_access=True,
-            block_public_access=aws_s3.BlockPublicAccess(
+            block_public_access=BlockPublicAccess(
                 block_public_acls=False,
                 block_public_policy=False,
                 ignore_public_acls=False,
@@ -59,28 +64,50 @@ class StacFastApiGeoparquetStack(Stack):
             ),
         )
 
+        # make the bucket public, requester-pays
         bucket.add_to_resource_policy(
-            aws_iam.PolicyStatement(
+            PolicyStatement(
                 actions=["s3:GetObject"],
                 resources=[bucket.arn_for_objects("*")],
-                principals=[aws_iam.AnyPrincipal()],
-                effect=aws_iam.Effect.ALLOW,
+                principals=[AnyPrincipal()],
+                effect=Effect.ALLOW,
             )
         )
 
-        self.add_requester_pays_to_s3_bucket(bucket=bucket)
+        add_request_pay = AwsSdkCall(
+            action="putBucketRequestPayment",
+            service="S3",
+            region=self.region,
+            parameters={
+                "Bucket": bucket.bucket_name,
+                "RequestPaymentConfiguration": {"Payer": "Requester"},
+            },
+            physical_resource_id=PhysicalResourceId.of(bucket.bucket_name),
+        )
+
+        aws_custom_resource = AwsCustomResource(
+            self,
+            "RequesterPaysCustomResource",
+            policy=AwsCustomResourcePolicy.from_sdk_calls(
+                resources=[bucket.bucket_arn]
+            ),
+            on_create=add_request_pay,
+            on_update=add_request_pay,
+        )
+
+        aws_custom_resource.node.add_dependency(bucket)
 
         CfnOutput(self, "BucketName", value=bucket.bucket_name)
 
-        api_lambda = aws_lambda.Function(
+        api_lambda = Function(
             scope=self,
             id="lambda",
             runtime=runtime,
             handler="handler.handler",
             memory_size=config.memory,
-            log_retention=aws_logs.RetentionDays.ONE_WEEK,
+            log_retention=RetentionDays.ONE_WEEK,
             timeout=Duration.seconds(config.timeout),
-            code=aws_lambda.Code.from_docker_build(
+            code=Code.from_docker_build(
                 path=os.path.abspath("../.."),
                 file="infrastructure/aws/lambda/Dockerfile",
                 build_args={
@@ -122,35 +149,6 @@ class StacFastApiGeoparquetStack(Stack):
 
         assert stage.url
         CfnOutput(self, "ApiURL", value=stage.url)
-
-    def add_requester_pays_to_s3_bucket(
-        self, bucket: aws_s3.IBucket
-    ) -> custom_resources.AwsCustomResource:
-        add_request_pay = custom_resources.AwsSdkCall(
-            action="putBucketRequestPayment",
-            service="S3",
-            region=self.region,
-            parameters={
-                "Bucket": bucket.bucket_name,
-                "RequestPaymentConfiguration": {"Payer": "Requester"},
-            },
-            physical_resource_id=custom_resources.PhysicalResourceId.of(
-                bucket.bucket_name
-            ),
-        )
-
-        aws_custom_resource = custom_resources.AwsCustomResource(
-            self,
-            "RequesterPaysCustomResource",
-            policy=custom_resources.AwsCustomResourcePolicy.from_sdk_calls(
-                resources=[bucket.bucket_arn]
-            ),
-            on_create=add_request_pay,
-            on_update=add_request_pay,
-        )
-
-        aws_custom_resource.node.add_dependency(bucket)
-        return aws_custom_resource
 
 
 app = App()
