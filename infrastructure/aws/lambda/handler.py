cdb0e4b37782ccba26826c458f3cca57a933553f
Pete Gadomski <pete.gadomski@gmail.com>
2025-03-13 12:28:00 -0600

    fix: very duct tape

--- infrastructure/aws/lambda/handler.py
+++ infrastructure/aws/lambda/handler.py
@@ -1,9 +1,14 @@
 """AWS Lambda handler."""
 
 import logging
+import os
+import shutil
 
 from mangum import Mangum
 from stac_fastapi.geoparquet.main import app
+
+if not os.path.exists("/tmp/duckdb-extensions") and os.path.isdir("/duckdb-extensions"):
+    shutil.copytree("/duckdb-extensions", "/tmp/duckdb-extensions")
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
