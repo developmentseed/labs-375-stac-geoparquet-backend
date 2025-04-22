869c40429c484248cda72bf0398c49493d3461b1
Pete Gadomski <pete.gadomski@gmail.com>
2025-04-22 17:05:29 -0600

    chore: put extensions on lambda

--- infrastructure/aws/lambda/handler.py
+++ infrastructure/aws/lambda/handler.py
@@ -1,16 +1,19 @@
 """AWS Lambda handler."""
 
 import logging
-import os
-import shutil
 
+import stac_fastapi.geoparquet.api
 from mangum import Mangum
-from stac_fastapi.geoparquet.main import app
-
-if not os.path.exists("/tmp/duckdb-extensions") and os.path.isdir("/duckdb-extensions"):
-    shutil.copytree("/duckdb-extensions", "/tmp/duckdb-extensions")
+from rustac import DuckdbClient
 
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
 
-handler = Mangum(app, lifespan="on")
+duckdb_client = DuckdbClient(
+    install_spatial=False,
+    use_hive_partitioning=False,
+    extension_directory="/asset/duckdb-extensions",
+)
+duckdb_client.execute("CREATE SECRET (TYPE S3, PROVIDER CREDENTIAL_CHAIN)")
+api = stac_fastapi.geoparquet.api.create(duckdb_client=duckdb_client)
+handler = Mangum(api.app, lifespan="on")
