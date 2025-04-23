45dd8424de3532b616ff4da2386375bb6e45222d
Pete Gadomski <pete.gadomski@gmail.com>
2025-04-23 07:58:11 -0600

    fix: extension directory

--- infrastructure/aws/lambda/handler.py
+++ infrastructure/aws/lambda/handler.py
@@ -1,6 +1,7 @@
 """AWS Lambda handler."""
 
 import logging
+import os
 from pathlib import Path
 
 import stac_fastapi.geoparquet.api
@@ -10,10 +11,12 @@ from rustac import DuckdbClient
 logging.getLogger("mangum.lifespan").setLevel(logging.ERROR)
 logging.getLogger("mangum.http").setLevel(logging.ERROR)
 
+extension_directory = Path(os.environ["LAMBDA_TASK_ROOT"]) / "duckdb-extensions"
+
 duckdb_client = DuckdbClient(
     install_spatial=False,
     use_hive_partitioning=False,
-    extension_directory=Path("/asset/duckdb-extensions"),
+    extension_directory=extension_directory,
 )
 duckdb_client.execute("CREATE SECRET (TYPE S3, PROVIDER CREDENTIAL_CHAIN)")
 api = stac_fastapi.geoparquet.api.create(duckdb_client=duckdb_client)
